1a5c2a387dad2dc37fac7c85257763e7d498ca9d
DC\fh059 <estradac@t-sytems.com>
2023-05-09 17:28:04 -0300

    Add project files.

---
+++ GithubPagesBlazor/Users/UserPage.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GithubPagesBlazor.Users
+{
+    public partial class UserPage
+    {
+        [Inject]
+        public UserRepository _userRepository { get; set; }
+
+        private IList<User> users = new List<User>();
+
+        protected override Task OnInitializedAsync()
+        {
+            LoadUsers();
+            return Task.CompletedTask;
+        }
+
+        void LoadUsers()
+        {
+            users = _userRepository.Users.FindAll().ToList();
+        }
+
+        void DeleteUser(int userId)
+        {
+            _userRepository.Delete(userId);
+            LoadUsers();
+        }
+
+        void InsertUser()
+        {
+            _userRepository.Insert(new User { FirstName = "first name", LastName = "last name", Email = "test@test.com" });
+            LoadUsers();
+        }
+    }
+}
---
+++ GithubPagesBlazor/Users/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using LiteDB;
+
+namespace GithubPagesBlazor.Users
+{
+    public class UserRepository
+    {
+        readonly LiteDatabase _db;
+
+        public UserRepository()
+        {
+            _db = new LiteDatabase("Filename=githubPages.db;connection=shared");
+            Users.EnsureIndex(x => x.Id);
+        }
+
+        public ILiteCollection<User> Users => _db.GetCollection<User>();
+
+        public User GetById(int id) => Users.FindById(id);
+
+        public void Insert(User user) => Users.Insert(user);
+
+        public void Delete(int id) => Users.Delete(id);
+    }
+}
